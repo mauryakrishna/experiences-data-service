67e7229e8bad1d64ad811af4dbdd990c14e8c069
mauryakrishna <mauryakrishna1@gmail.com>
2020-06-23 23:27:26 +0530

    uid

--- migrations/sqls/20200517131811-authors-up.sql
+++ migrations/sqls/20200517131811-authors-up.sql
@@ -1,6 +1,10 @@
 /* Replace with your SQL commands */
 CREATE TABLE `authors` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  -- uid: uinque string, made of username from user's emailid if thats enought else append some random number to make it one
+  -- eg: uid:mauryakrishna1, from mauryakrishna1@gmail.com
+  -- uid: used for mention and author profile url loading
+  `uid` varchar(100) NOT NULL,
   `displayname` varchar(100) NOT NULL,
   `email` varchar(1000) NOT NULL,
   `shortintro` varchar(1000) DEFAULT NULL,
