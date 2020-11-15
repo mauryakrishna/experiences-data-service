1213a81c1de8fc7d81ee987d465e5a9280668e37
Krishna Maurya <mauryakrishna1@gmail.com>
2020-11-15 13:49:09 +0530

    Revert "mysql connection pooling"
    This reverts commit 9b48b890310fa25565dd3dc576a6162a64403d3e.

--- src/connectors/mysql.js
+++ src/connectors/mysql.js
@@ -4,10 +4,10 @@ import mysql from 'mysql2';
 /**
  * The below env variable check is to avoid the timewasting may happen because of DB
  * connection refuse.
- */
+ */ 
 const { MYSQL_USER, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_CONNECTION_LIMIT } = process.env;
 
-if (!MYSQL_HOST || !MYSQL_USER || !MYSQL_PASSWORD || !MYSQL_DATABASE || !MYSQL_CONNECTION_LIMIT) {
+if (!MYSQL_HOST || !MYSQL_USER || !MYSQL_PASSWORD || !MYSQL_DATABASE || !MYSQL_CONNECTION_LIMIT) { 
   console.log(`User:${MYSQL_USER}, Host:${MYSQL_HOST}, Pwd:${MYSQL_PASSWORD}, Database:${MYSQL_DATABASE}, ConnectionLimit${MYSQL_CONNECTION_LIMIT}`);
   throw Error('DB ENV variables are not loading.');
 }
@@ -21,20 +21,16 @@ const mysqlconfig = {
   connectionLimit: process.env.MYSQL_CONNECTION_LIMIT
 };
 
-const pool = mysql.createPool(mysqlconfig);
+const conn = mysql.createConnection(mysqlconfig);
 
-/*
-  Below code is just to ensure that mysql connection is happening correctly
-*/
-pool.getConnection(function (err, connection) {
+conn.connect(function(err) {
   if (err) {
     console.error(`Error connecting with user ${MYSQL_USER} and error ${err}`);
-  } else {
-    console.log(`Connection established, thead id ${connection.threadId}.`);
-    connection.release();
+    return;
   }
+  console.log(`Connection established, thead id ${conn.threadId}.`);
 });
 
-const pomisePool = pool.promise();
+const query = util.promisify(conn.query).bind(conn);
 
-export default { query: pomisePool.query };
+export default { query };
