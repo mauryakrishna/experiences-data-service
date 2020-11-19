589ba69f08e619eb852b034579ba8c7a69f72287
Krishna Maurya <mauryakrishna1@gmail.com>
2020-11-19 21:00:34 +0530

    param correction and expiry info of 24 hours in verification email

--- src/resolvers/authors.js
+++ src/resolvers/authors.js
@@ -9,7 +9,7 @@ import { cursorFormat, createdAtFormat, publishDateFormat, addMinutesInCurrentTi
 export const verifyMe = (_, __, context) => {
   const { displayname, authoruid } = context;
   const valid = !!(displayname && authoruid);
-  if (valid) { 
+  if (valid) {
     return { valid, displayname, authoruid };
   }
   return { valid };
@@ -19,7 +19,7 @@ export const verifyMe = (_, __, context) => {
 // get Authors details along both published experiences and unpublished experiences
 // itsme: true - when author himself visit the page 
 // need to know if its authors own or is visitng different author page, difference would be not showing experiences in draft 
-export const getAuthor = async (_, { cursor, experienceperpage, uid, itsme }, context) => { 
+export const getAuthor = async (_, { cursor, experienceperpage, uid, itsme }, context) => {
   cursor = cursor || cursorFormat(new Date());
   experienceperpage = experienceperpage || EXPERIENCES_PER_PAGE;
   itsme = (uid == context.authoruid);
@@ -28,16 +28,16 @@ export const getAuthor = async (_, { cursor, experienceperpage, uid, itsme }, co
     SELECT *
     FROM experiences
     WHERE authoruid = ? AND updated_at < ?
-    ${(itsme ? '': 'AND ispublished=true')} 
+    ${(itsme ? '' : 'AND ispublished=true')} 
     ORDER BY updated_at DESC
     LIMIT ?
   `;
 
   const experiencesResult = await mysql.query(experiencesQuery, [uid, cursor, experienceperpage]);
-  
+
   const len = experiencesResult.length;
-  
-  if (len > 0) { 
+
+  if (len > 0) {
     cursor = cursorFormat(new Date(experiencesResult[len - 1].updated_at));
   }
 
@@ -46,20 +46,20 @@ export const getAuthor = async (_, { cursor, experienceperpage, uid, itsme }, co
   const result = await mysql.query(query, [uid]);
 
   const author = result[0];
-  author.experiences = experiencesResult.map((exp) => { 
+  author.experiences = experiencesResult.map((exp) => {
     exp.created_at = createdAtFormat(exp.created_at);
-    
-    if (exp.publishdate) { 
+
+    if (exp.publishdate) {
       exp.publishdate = publishDateFormat(exp.publishdate);
     }
-    
+
     return exp;
   });
-  
+
   return { cursor, author };
 };
 
-const getUniqueUid = async (username) => { 
+const getUniqueUid = async (username) => {
 
   const query = `
     SELECT uid FROM authors
@@ -67,13 +67,13 @@ const getUniqueUid = async (username) => {
   `;
 
   const result = await mysql.query(query);
-  
+
   let uid = username;
   const usernameset = result.map((user) => { return user.uid });
-  
+
   if (usernameset.includes(username)) {
     // if found means uid duplicate, so generate unique
-    do { 
+    do {
       let randomNumber = Math.floor(Math.random(3) * 1000); // 3 digit 
       uid = `${username}${randomNumber}`;
     }
@@ -83,28 +83,28 @@ const getUniqueUid = async (username) => {
   return uid;
 }
 
-const getExisitingAuthor = async (email) => { 
+const getExisitingAuthor = async (email) => {
   const query = `SELECT email, isemailverified FROM authors WHERE email = ?`;
 
   const result = await mysql.query(query, [email]);
-  
-  if (result && result.length) { 
-    return {exist: true, isemailverified: result[0].isemailverified}
+
+  if (result && result.length) {
+    return { exist: true, isemailverified: result[0].isemailverified }
   }
-  return {exist: false};
+  return { exist: false };
 }
 
 /**
  * because fb Oauth is not used, below methodis not in force
  */
-export const buttonPressRegister = async (_, __, context) => { 
+export const buttonPressRegister = async (_, __, context) => {
   const { displayname, email } = context;
   const variables = { input: { displayname, email } };
 
   return await signupAuthor(_, variables, context);
 }
 
-const setForAccountVerification = async (displayname, email) => { 
+const setForAccountVerification = async (email, displayname) => {
   // after inserting a user, set for email verification
   const verificationkey = getAlphanumeric();
   const verificationQuery = `
@@ -113,24 +113,24 @@ const setForAccountVerification = async (displayname, email) => {
   `;
 
   const verifytracker = await mysql.query(verificationQuery, [email, verificationkey, addMinutesInCurrentTime(VERIFICATION_LINK_EXPIRY_TIME)]);
-  
+
   //send mail for verifying email address
   await SendEmailVerificationLink(displayname, email, verificationkey);
 };
 
-export const resendVerificationLink = async (_, { email }, context) => { 
+export const resendVerificationLink = async (_, { email }, context) => {
   await setForAccountVerification(email);
   return { resendsuccess: true };
 }
 
 // kind of register user
 export const signupAuthor = async (_, { input }, context) => {
-  
+
   const { displayname, email, password, shortintro, region, languages } = input;
 
   // the below is just backend protection from crreatng a duplicate author
   const { exist, isemailverified } = await getExisitingAuthor(email);
-  
+
   // if found already, this should never happen that while registering we found if the user for 
   // given email exist, it will be done before reaching this point
   if (exist) {
@@ -165,19 +165,18 @@ export const signinAuthor = async (_, { email, password }, context) => {
     FROM authors
     WHERE email=?
   `;
-  
+
   const result = await mysql.query(query, [email]);
   const author = result[0];
-  
+
   // user may not have signed up
-  if (!result || !result.length) { 
+  if (!result || !result.length) {
     return { exist: false };
   }
-  else 
-  { 
+  else {
     const { isemailverified } = result[0];
     // for asking to validate email
-    if (!isemailverified) { 
+    if (!isemailverified) {
       return { exist: true, isemailverified };
     }
 
@@ -192,7 +191,7 @@ export const signinAuthor = async (_, { email, password }, context) => {
         exist: true, author: { ...author, authoruid: author && author.authoruid }, token, isemailverified
       }
     }
-    else { 
+    else {
       return {
         exist: true, message: "Authentication failed.", isemailverified
       };
