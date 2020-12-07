eb1ef6f76c9ebe49df494979debdefd1f51c0a81
Krishna Maurya <mauryakrishna1@gmail.com>
2020-12-07 23:52:31 +0530

    refresh token expiry increased

--- src/config/constants.js
+++ src/config/constants.js
@@ -9,6 +9,6 @@ export const FORGOT_PASSWORD_LINK_EXPIRY_TIME = 60; // in minutes, amounts to 1 
 
 export const VERIFICATION_LINK_EXPIRY_TIME = 24 * 60 // in minutes, amounts to 24 hours
 
-export const REFRESH_TOKEN_EXPIRY = 3000 * 24 * 60 * 1000 // in seconds, approx 10 years
+export const REFRESH_TOKEN_EXPIRY = 3000 * 24 * 60 * 60 * 1000 // in seconds, approx 10 years
 
 export const ACCESS_TOKEN_EXPIRY = 24 * 60 // in minutes, amounts to 1 day
