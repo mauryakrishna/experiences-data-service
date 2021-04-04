90cd694d374390a31efb74b914d2d73eacf11bc7
Krishna Maurya <mauryakrishna1@gmail.com>
2021-04-04 13:55:29 +0530

    added condition for null handling

--- src/utils/getexperienceintrotext.js
+++ src/utils/getexperienceintrotext.js
@@ -4,7 +4,7 @@ const FEATURED_TEXT_LENGTH = 200;
 
 const serialize = nodes => {
   let text = ""
-  for(const node of nodes) {
+  for(const node of nodes || []) {
     text += Node.string(node)
     if(text.length>FEATURED_TEXT_LENGTH) {
       text = `${text.slice(0, FEATURED_TEXT_LENGTH).trim()}...`
