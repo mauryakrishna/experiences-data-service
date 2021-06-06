5a443b6c4b2acd4f28cb33227b3b0c78ccf1ce92
Krishna Maurya <mauryakrishna1@gmail.com>
2021-06-06 13:21:33 +0530

    intro text spacing correction

--- src/utils/getexperienceintrotext.js
+++ src/utils/getexperienceintrotext.js
@@ -3,9 +3,11 @@ import { Node } from 'slate';
 const FEATURED_TEXT_LENGTH = 140;
 
 const serialize = nodes => {
+  let nodeString = []
   let text = ""
   for(const node of nodes || []) {
-    text += Node.string(node)
+    nodeString.push(Node.string(node))
+    text = nodeString.join(" ")
     if(text.length>FEATURED_TEXT_LENGTH) {
       text = `${text.slice(0, FEATURED_TEXT_LENGTH).trim()}...`
       break;
